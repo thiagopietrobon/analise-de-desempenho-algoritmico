60103578802001c79e0b23de9170db1345ba9474
Thiago Henrique <thiagotx678@gmail.com>
2026-09-09 10:33:57 -0300

    alteração para exportação de dados e graficos automatica

--- scr/main.py
+++ scr/main.py
@@ -1,13 +1,19 @@
 import sys
-from scr.medicoes import run_benchmark
+from medicoes import run_benchmark
+from export import export_to_csv
 from plotagem import plot_results
 
-# Ajuste global de recursão necessário para o pior caso do Pivô Fixo
 sys.setrecursionlimit(200000)
 
 SIZES = [1000, 2000, 4000, 8000, 16000]
 SEED = 42
 
+DATA_DIR = "dados"
+GRAPHS_DIR = "graficos"
+
 if __name__ == "__main__":
-    results = run_benchmark(SIZES, seed=SEED)
-    plot_results(SIZES, results)
+    plot_data, raw_rows = run_benchmark(SIZES, seed=SEED)
+    
+    # Exportação para subpastas
+    export_to_csv(raw_rows, output_dir=DATA_DIR, filename="quicksort_resultados.csv")
+    plot_results(SIZES, plot_data, output_dir=GRAPHS_DIR, filename="quicksort_comparação.png")
